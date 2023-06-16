be87af49c836b62eb21102eb9273036cdb9e1683
Basil Latif <blatif@ranker.com>
2023-06-15 19:37:30 -0700

    added generator code

--- book_code/chap2-generators.py
+++ book_code/chap2-generators.py
@@ -1,2 +1,22 @@
 numbers = [7, 4, 11, 3]
-iter(numbers)
+#iter() is a generator method
+#<list_iterator object at 0x10b473e20>
+print(iter(numbers))
+print('')
+names = ["Tom", "Shelly", "Garth"] 
+names_it = iter(names)
+print("next(names_it):",  next(names_it))
+print("next(names_it):",  next(names_it))
+print("next(names_it):",  next(names_it))
+
+#2.4 Python is filled with Iterators
+calories = { "apple": 95,
+"slice of bacon": 43, "cheddar cheese": 113,
+"ice cream": 15, # You wish!
+}
+items = calories.items()
+print('type(items)', type(items))
+len(items)
+calories['orange'] = 50
+print(('orange', 50) in items)
+print(('orange', 20) in items)
