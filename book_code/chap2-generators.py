4158d2c900e9e89d7fbd98ceb8bbfde56354cf14
Basil Latif <blatif@ranker.com>
2023-06-15 20:43:31 -0700

    map, filter, zip

--- book_code/chap2-generators.py
+++ book_code/chap2-generators.py
@@ -16,19 +16,47 @@ calories = { "apple": 95,
 }
 items = calories.items()
 print('type(items)', type(items))
-len(items)
+print("len(items):", len(items))
+print("items: ", items)
 calories['orange'] = 50
-print(('orange', 50) in items)
-print(('orange', 20) in items)
+print("items: ", items)
+print( '(\'orange\', 50) in items::', ('orange', 50) in items)
+print( '(\'orange\', 20) in items::', ('orange', 20) in items)
 
 foods = calories.keys() 
 counts = calories.values()
+print('foods: ', foods)
+print('counts: ', counts)
+
 #False
-print('yogurt' in foods))
+print('\'yogurt\' in foods:::', 'yogurt' in foods)
 #False
-print(100 in counts)
+print('100 in counts:::',100 in counts)
 calories['yogurt'] = 100 
-#True
-print('yogurt' in foods)
-#True
-print(100 in counts)
+print("calories: ", calories)
+print('\'yogurt\' in foods:::','yogurt' in foods)
+print('100 in counts:::',100 in counts)
+
+#Iteration has snuck into many places in Python. The built-in range function returns an iterable:
+seq = range (3)
+print('type(seq): ', type(seq))
+for n in seq: print(n)
+
+
+#The built-in map, filter, and zip functions all return iterators:
+numbers = [1,2,3]
+big_numbers = [100,200,300]
+def double(n):
+    return 2*n
+def is_even(n):
+    return n % 2 == 0 
+mapped = map(double, numbers)
+print('mapped: ', mapped)
+for num in mapped: print(num)
+
+filtered = filter(is_even , numbers)
+for num in filtered: print(num)
+
+zipped = zip(numbers , big_numbers)
+print('zipped:', zipped)
+for pair in zipped: print(pair)
