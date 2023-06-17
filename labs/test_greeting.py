eab1990a19e62735d5b609c96725add3d75922a3
Basil Latif <blatif@ranker.com>
2023-06-17 15:34:49 -0700

    updates to lab

--- labs/test_greeting.py
+++ labs/test_greeting.py
@@ -3,4 +3,14 @@ from greeting import greet
 
 class TestGreeting(unittest.TestCase):
     def test_greet(self):
-        pass
+        greet("John")
+        #assert "Hi, John" == greet("John")
+        self.assertEqual("Hi, John", greet("John"))
+
+        """
+        * Where is the test module ("test_greeting") specified?
+        * Where is the test class ("TestGreeting") specified?
+        * Where is the test method ("test_greet") specified?
+        * What is the line number of the failing test?
+        """
+
