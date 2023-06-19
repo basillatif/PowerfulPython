a22c4a8bad4f247733d0955e5b17c1444120f106
Basil Latif <blatif@ranker.com>
2023-06-19 09:53:37 -0700

    hi

--- labs/test_greeting.py
+++ labs/test_greeting.py
@@ -1,16 +1,20 @@
 import unittest
 from greeting import greet
+"""
+    * Where is the test module ("test_greeting") specified?
+        in the test_greeting file
+    * Where is the test class ("TestGreeting") specified?
+        in the test_greeting file
+    * Where is the test method ("test_greet") specified?
+            in the test_greeting file
+
+    * What is the line number of the failing test?
+    18
+"""
 
 class TestGreeting(unittest.TestCase):
     def test_greet(self):
         greet("John")
         #assert "Hi, John" == greet("John")
         self.assertEqual("Hi, John", greet("John"))
-
-        """
-        * Where is the test module ("test_greeting") specified?
-        * Where is the test class ("TestGreeting") specified?
-        * Where is the test method ("test_greet") specified?
-        * What is the line number of the failing test?
-        """
 
